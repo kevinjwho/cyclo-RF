1697fb1472d7ac442e3816f18fe7de67bf2de81e
kevinjwho <kevin.jw.ho@gmail.com>
2019-04-24 23:25:02 -0400

    script to find amplifier params

---
+++ auto-tuner/offset_gain.m
@@ -0,0 +1,20 @@
+%% offset gain amplifier
+
+%% 1. Introduction - voltage level inputs
+Vref = 9; % voltage level of stable reference
+Voutfs = 5; % full-scale output voltage
+Voutzs = 0; % zero-scale output voltage
+Vinfs = 1.1; % full-scale input ovltage
+Vinzs = -1.1; % zero-scale input voltage
+
+%% 2. Determining the type of function - solving for m and b
+m = (Voutfs - Voutzs)/(Vinfs - Vinzs)
+b = Voutzs - m*Vinzs
+
+%% 3. Positive m and positive b
+
+% -- inputs
+R1 = 22000; % choose R1
+Rf = 22000; % choose Rf
+R2 = Vref * R1 * m / b % calculate R2
+Rg = R2 * Rf/ (m*(R1+R2)-R2) % calculate Rg
