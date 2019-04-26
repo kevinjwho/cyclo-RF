dbd9baf2156160d43e03eed3195d1c57c8b9c62b
kevinjwho <kevin.jw.ho@gmail.com>
2019-04-25 20:33:50 -0400

    phase sweep written, pending testing

--- pfag_main.m
+++ pfag_main.m
@@ -8,13 +8,18 @@ p = tuner_initialize_pfag; disp('pfag initialized');
 
 %% make arbitrary sine waveform
 clc
+numpts = 47;
+t = linspace(0,2*pi,numpts);
+p.data1 = sin(t);
 for theta = 0:5:360 % in degrees
-    p.data = sin(linspace(0,2*pi,47)+theta*pi/180); % convert to rad
-    %fprintf(p.deviceObj,':FUNC:USER');
-    %fprintf(p.deviceObj,':FUNC USER');
-    str = [':DATA VOLATILE, ',pfag_arr2str(p.data)];
+    p.data2 = sin(t+theta*pi/180); % convert to rad
+    %fprintf(p.deviceObj,':FUNC2:USER');
+    %fprintf(p.deviceObj,':FUNC2 USER');
+    str = [':DATA2 VOLATILE, ',pfag_arr2str(p.data2)];
     %fprintf(p.deviceObj,str);
-    figure(1), stem(p.data), 
+    figure(1), stem(t, p.data1); hold on,
+    stem(t, p.data2); hold off;
+    grid, legend('OUT1', 'OUT2');
     title(['theta = ' num2str(theta)]), pause(1/10)
 end
 
